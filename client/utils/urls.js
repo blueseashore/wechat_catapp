57d4034e84bc923fbe3d2cfb990326070a49affe
blueseashore <455019825@qq.com>
2018-08-02 12:03:37 +0800

    调试

--- client/utils/urls.js
+++ client/utils/urls.js
@@ -3,13 +3,13 @@
  * @module urls
  * @author uckendo <455019825@qq.com>
  */
-const HOST = 'https://gitee.normalcoder.com/';
+const HOST = 'https://miniapp.uckendo.com/';
 const urls = {
     host: HOST,
     index: HOST + '/Index/index',
     UserLogin: HOST + '/User/login', //小程序用户登陆模块
     UserUpdateInfo: HOST + '/User/UserUpdateInfo',
-    BulletType: HOST + '/index/BulletType', //获取动弹类型列
+    BulletType: HOST + '/index/category', //获取动弹类型列
     blogDetail: HOST + '/Blog/detail', //获取动弹类型列
     mineList: HOST + '/System/mineList', //我的列表
 };
--- server/application/controllers/Index.php
+++ server/application/controllers/Index.php
@@ -15,10 +15,10 @@ class Index extends CI_Controller
     public function category()
     {
         $response['scroll'] = false;
-        $list[] = ['id' => 'weekly', 'title' => '码云周刊', 'url' => '/Blog/category?category=weekly', 'data' => []];
-        $list[] = ['id' => 'tips', 'title' => '用户故事', 'url' => '/Blog/category?category=tips', 'data' => []];
-        $list[] = ['id' => 'cases', 'title' => '企业案例', 'url' => '/Blog/category?category=cases', 'data' => []];
-        $list[] = ['id' => 'updates', 'title' => '产品动态', 'url' => '/Blog/category?category=updates', 'data' => []];
+        $list[] = ['id' => 'weekly', 'title' => '码云周刊', 'url' => '/article/list?category=weekly', 'data' => []];
+        $list[] = ['id' => 'tips', 'title' => '用户故事', 'url' => '/article/list?category=tips', 'data' => []];
+        $list[] = ['id' => 'cases', 'title' => '企业案例', 'url' => '/article/list?category=cases', 'data' => []];
+        $list[] = ['id' => 'updates', 'title' => '产品动态', 'url' => '/article/list?category=updates', 'data' => []];
         $response['list'] = $list;
         $response['selectedId'] = 0;
         send_json(TRUE, $response);
