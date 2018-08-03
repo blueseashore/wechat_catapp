5527854cf2aa66f6368845609bdd93bbe30dbd97
blueseashore <455019825@qq.com>
2018-08-03 16:54:17 +0800

    文章内容

--- client/utils/urls.js
+++ client/utils/urls.js
@@ -9,9 +9,9 @@ const urls = {
     index: HOST + '/Index/index',
     UserLogin: HOST + '/User/login', //小程序用户登陆模块
     UserUpdateInfo: HOST + '/User/UserUpdateInfo',
-    BulletType: HOST + '/index/category', //获取动弹类型列
-    blogDetail: HOST + '/article/detail', //获取文章内容
-    mineList: HOST + '/System/mineList', //我的列表
+    BulletType: HOST + '/Index/category', //获取动弹类型列
+    blogDetail: HOST + '/Article/detail', //获取文章内容
+    mineList: HOST + '/User/mineList', //我的列表
 };
 module.exports = {
     urls
